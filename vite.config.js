ea23dcca2a205655af583fa554984e31d30816ef
raquelpacheco007 <raquel.p.pereira@gmail.com>
2025-08-28 14:16:59 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Substitua "NOME-DO-REPOSITORIO" pelo nome exato do repositório no GitHub
-export default defineConfig({
-  plugins: [react()],
-  base: "/geradordeplacas/",
-})
